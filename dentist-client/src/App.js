1e8335447e030f77f52af4f9d57b519f385db49c
jayveeannsoriano <2190860@slu.edu.ph>
2022-09-10 02:25:52 +0800

    converted landing page. icons to be fixed later

--- dentist-client/src/App.js
+++ dentist-client/src/App.js
@@ -1,15 +1,26 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 import LandingPage from "./pages/landing-page";
 
+// fontawesome
+// library.add(fab) 
 
 function App() {
+  // AOS animation
+  useEffect(() => {
+    AOS.init({duration: 1000});
+    AOS.refresh();
+    }, []);
+
+  // Pages
   return(
     <div className="App" >
       < LandingPage />
+
     </div>
     )
 
