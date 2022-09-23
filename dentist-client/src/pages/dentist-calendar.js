23e04e3acac302746031da524ab5bd4049c7038f
Mr-Benzedrine <2194280@slu.edu.ph>
2022-09-23 13:26:50 +0800

    Created js script for dentist calendar calendar

--- dentist-client/src/pages/dentist-calendar.js
+++ dentist-client/src/pages/dentist-calendar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {useEffect} from "react";
 
 
 function DentistCalendar(){
@@ -18,6 +17,88 @@ function DentistCalendar(){
             <script src='Calendar/fullcalendar/packages/daygrid/main.js'></script>
             <script src='Calendar/fullcalendar/packages/timegrid/main.js'></script>
             <script src='Calendar/fullcalendar/packages/list/main.js'></script>
+
+            <script src="dentist-client/src/pages/CalendarScript.js"></script>
+
+            {/*<script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    var calendarEl = document.getElementById('calendar');
+
+                    var calendar = new FullCalendar.Calendar(calendarEl, {
+                    plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list' ],
+                    height: 'parent',
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                    },
+
+                    defaultView: 'dayGridMonth',
+                    defaultDate: '2020-02-12',
+                    navLinks: true, // can click day/week names to navigate views
+                    editable: true,
+                    eventLimit: true, // allow "more" link when too many events
+                    events: [
+                        {
+                            title: 'All Day Event',
+                            start: '2020-02-01',
+                        },
+                        {
+                            title: 'Long Event',
+                            start: '2020-02-07',
+                            end: '2020-02-10'
+                        },
+                        {
+                            groupId: 999,
+                            title: 'Repeating Event',
+                            start: '2020-02-09T16:00:00'
+                        },
+                        {
+                            groupId: 999,
+                            title: 'Repeating Event',
+                            start: '2020-02-16T16:00:00'
+                        },
+                        {
+                            title: 'Conference',
+                            start: '2020-02-11',
+                            end: '2020-02-13'
+                        },
+                        {
+                            title: 'Meeting',
+                            start: '2020-02-12T10:30:00',
+                            end: '2020-02-12T12:30:00'
+                        },
+                        {
+                            title: 'Lunch',
+                            start: '2020-02-12T12:00:00'
+                        },
+                        {
+                            title: 'Meeting',
+                            start: '2020-02-12T14:30:00'
+                        },
+                        {
+                            title: 'Happy Hour',
+                            start: '2020-02-12T17:30:00'
+                        },
+                        {
+                            title: 'Dinner',
+                            start: '2020-02-12T20:00:00'
+                        },
+                        {
+                            title: 'Birthday Party',
+                            start: '2020-02-13T07:00:00'
+                        },
+                        {
+                            title: 'Click for Google',
+                            url: 'http://google.com/',
+                            start: '2020-02-28'
+                        }
+                    ]
+                });
+
+            calendar.render();
+         });
+        </script> */}
 
             
             <script src="Calendar/js/main.js"></script>
