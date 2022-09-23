1692fda634a1fcbf646aca21f41199dd7a86a24e
Mr-Benzedrine <2194280@slu.edu.ph>
2022-09-23 10:36:50 +0800

    Updated dentist calendar

--- dentist-client/src/components/header.jsx
+++ dentist-client/src/components/header.jsx
@@ -18,6 +18,26 @@ function Header() {
             {/*Google Fonts*/}
             <link href="https://fonts.gstatic.com" rel="preconnect"/>
             <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"/>
+            
+            {/*Calendar Fonts*/}
+            <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet"/>
+            <link rel="stylesheet" href="Calendar/fonts/icomoon/style.css"/>
+            <link href='Calendar/fullcalendar/packages/core/main.css' rel='stylesheet'/>
+            <link href='Calendar/fullcalendar/packages/daygrid/main.css' rel='stylesheet'/>
+
+            {/* Calendar Bootstrap CSS */}
+            <link rel="stylesheet" href="Calendar/css/bootstrap.min.css"/>
+            <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+            <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"/>
+            <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"/>
+            <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet"/>
+            <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet"/>
+            <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet"/>
+            <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet"/>
+
+            {/* Calendar Style */}
+            <link rel="stylesheet" href="Calendar/css/style.css"/>
 
             {/*Vendor CSS Files*/}
             <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
--- dentist-client/src/pages/dentist-calendar.js
+++ dentist-client/src/pages/dentist-calendar.js
@@ -3,6 +3,12 @@ import React from "react";
 function DentistCalendar(){
     return(
         <div>
+            
+            <div id='calendar-container'>
+                <div id='calendar'></div>
+            </div>
+
+            
 
         </div>
     )
