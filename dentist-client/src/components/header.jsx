81248a718c9b89ab584df3fbb4d3ce25c8b4fe42
Mr-Benzedrine <2194280@slu.edu.ph>
2022-10-10 22:48:00 +0800

    Update header.jsx

--- dentist-client/src/components/header.jsx
+++ dentist-client/src/components/header.jsx
@@ -50,6 +50,18 @@ function Header() {
 
             {/*Template Main CSS File*/}
             <link href="assets/css/style.css" rel="stylesheet"/>
+
+            {/* Vendor JS Files */}
+            <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+            <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+            <script src="assets/vendor/chart.js/chart.min.js"></script>
+            <script src="assets/vendor/echarts/echarts.min.js"></script>
+            <script src="assets/vendor/quill/quill.min.js"></script>
+            <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+            <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+
+            {/* Template Main JS File */}
+            <script src="assets/js/main.js"></script>
         
         </div>
     )
