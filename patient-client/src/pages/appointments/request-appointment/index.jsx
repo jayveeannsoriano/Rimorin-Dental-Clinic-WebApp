3d803de3db853597892b8b85d7db76999fb49ce8
Dabe Ventigan <2190962@slu.edu.ph>
2022-10-25 20:26:12 +0800

    Co-authored-by: Jayvee Ann Soriano <jayveeannsoriano@users.noreply.github.com> Co-authored-by: Quina Mae <QuinaMae@users.noreply.github.com>

--- patient-client/src/pages/appointments/request-appointment/index.jsx
+++ patient-client/src/pages/appointments/request-appointment/index.jsx
@@ -1,116 +1,70 @@
-import React from "react";
-import "../../../styles/booking.css";
+// SCRATCH!!!
+
+import React, {Component} from "react";
 import 'react-bootstrap';
 import Timeslot from "../../../components/timeslot.jsx";
-// import Stepper from 'bs-stepper'
+import BookingInput from './booking-input';
+import BookingDetail from "./booking-detail.jsx";
+import BookingConfirm from "./booking-confirm.jsx";
+import AppointmentPage from "../../appointments";
 
-const Booking = () => {
-    return(
-        <>
-                <nav>
-                    <ol className="breadcrumb">
-                        <li className="breadcrumb-item">
-                            <a href="/">Home</a>
-                        </li>
-                        <li className="breadcrumb-item">
-                            <a href="/appointments">Appointments</a>
-                        </li>
-                        <li className="breadcrumb-item active">Request Appointment</li>
-                    </ol>
-                </nav>
+class BookingMain extends Component {
+    state = {
+        step: 1,
+        startDate: "",
+        consulInput: "",
+        timeData:"",
+    }
 
-            <section className="section dashboard">
-                <div className="row">
-                
-                    {/* Card Title*/}
-                    <div className="card-body">
-                    <h5 className="appt-title">REQUEST APPOINTMENT</h5>
+    prevStep = () => {
+        const {step} = this.state;
+        this.setState ({ step: step - 1});
+    }
+    
+    nextStep = () => {
+        const {step} = this.state;
+        this.setState ({ step: step + 1});
+    }
 
-                    {/* Stepper */}
-                    <div className="md-stepper-horizontal orange">
-                        <div className="md-step active">
-                        <div className="md-step-circle"><span>1</span></div>
-                        <div className="md-step-title">Fill-up</div>
-                        <div className="md-step-bar-left"></div>
-                        <div className="md-step-bar-right"></div>
-                        </div>
-                        <div className="md-step">
-                        <div className="md-step-circle"><span>2</span></div>
-                        <div className="md-step-title">Review Appointment Details</div>
-                        {/* <div className="md-step-optional">Optional</div> */}
-                        <div className="md-step-bar-left"></div>
-                        <div className="md-step-bar-right"></div>
-                        </div>
-                        <div className="md-step">
-                        <div className="md-step-circle"><span>3</span></div>
-                        <div className="md-step-title">Done</div>
-                        <div className="md-step-bar-left"></div>
-                        <div className="md-step-bar-right"></div>
-                        </div>
-                    </div>
+    //handles the navigations to retain data past the prevStep
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value});
+        console.log(input);
+    }
 
-                    <div className="divider"></div>
+    render(){
+        const {step} = this.state;
+        const {startDate,consulInput,timeData} = this.state;
+        const values = {startDate,consulInput,timeData};
 
-                    <div className="doctor-info">
-                        <h1>DOCTOR INFORMATION</h1>
-                        <h2>Doctor Name:</h2>
-                        <h3 id="doctor-name">Pamela Rimorin Concepcion</h3>
-                        <h2>Clinic Location:</h2>
-                        <h3 id="clinic-location">Victoria Shoppesville, Upper Mabini Street, Baguio City, Philippines</h3>
-                    </div>
+        switch (step) {
+            case 1:
+                return (
+                    <BookingInput 
+                    nextStep = {this.nextStep}
+                    // handleChange = {this.handleChange}
+                    // values = {values}
+                    />
+                )
 
-                    {/* Booking deets */}
-                    <div className="appointment-form" id="appointment-form">
+            case 2:
+                return(
+                    <BookingDetail
+                    prevStep = {this.prevStep} 
+                    nextStep = {this.nextStep}
+                    />
+                )
 
-                    <form className="row g-3 needs-validation" noValidate/>
-                        <div className="col">
-                            <label htmlFor="validationCustom01" className="form-label">Select Appointment Date <span className="text-danger font-weight-bold">*</span></label>
-                            <input type="date" className="form-control" id="appointment-date" required/>
-                            <div className="valid-feedback">
-                                Looks good!
-                            </div>
-                        </div>
-
-                        <div className="col">
-                            <label htmlFor="validationCustom01" className="form-label">Select Time for Appointment <span className="text-danger font-weight-bold">*</span></label>
-                            <p> Available Times </p>
-                            <Timeslot/>
-                        </div>
-                    </div>
-
-                        <div className="col-12 reason-form">
-                            <label htmlFor="validationCustom01" className="form-label">Reason for Consultation <span className="text-danger font-weight-bold">*</span></label>
-                            <textarea className="form-control" id="reason" rows="5" placeholder="Write reason here..."></textarea>
-                            <div className="valid-feedback">
-                                Looks good!
-                            </div>
-                        </div>
-
-                        <div className="col-12">
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required/>
-                                <label className="form-check-label" htmlFor="invalidCheck">
-                                    Agree to the <a href="/">Terms and Conditions.</a>
-                                </label>
-                                <div className="invalid-feedback">
-                                    You must agree before proceeding.
-                                 </div>
-                            </div>
-                        </div>
-
-                        <div className="col-12">
-                            <div className="appt-bttns">
-                            <button className="btn btn-outline-secondary" type="submit">Cancel</button>
-                            <button className="btn btn-primary" type="submit">Next</button>
-                            </div>
-                        </div>
-
-                    </div> {/* End of card-body */}
-
-                </div>
-            </section>
-        </>
-    )
+            case 3:
+                return(
+                    <BookingConfirm
+                    nextStep = {this.nextStep}
+                    />
+                )
+            default:
+                break;
+        }
+    }
 }
 
-export default Booking;
+export default BookingMain;
