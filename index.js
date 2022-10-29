93c1311ffb53ab2ff810606153a4a64d637d3652
John Lloyd Balcita <2195929@slu.edu.ph>
2022-10-30 01:57:09 +0800

    initial routing of file

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ const PORT = process.env.PORT || 3001;
 
 const path = require("path");
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+// Serve any static files
+app.use(express.static(path.join(__dirname, "client/build")));
+// Handle React routing, return all requests to React app
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
+
 
 
 //server
