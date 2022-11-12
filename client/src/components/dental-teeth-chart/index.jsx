e6f506b39c8a31e0f9c509e82d07b69ca1c48ace
John Lloyd Balcita <2195929@slu.edu.ph>
2022-11-13 02:55:52 +0800

    added get request of dental chart data and marking after response

--- client/src/components/dental-teeth-chart/index.jsx
+++ client/src/components/dental-teeth-chart/index.jsx
@@ -3,6 +3,21 @@ import "./tooth.css";
 import "./chart.js";
 
 const DentalChart = ({handleClickTeeth}) => {
+    const email =  document.getElementById('emailaddress').textContent;
+    
+    const getDentalCharts = async() => {
+        const response = await axios.get('http://localhost:3001/getDentalChart', { params: { email: email } });
+        response.forEach(teeth => {
+            var el = document.getElementById(teeth);
+            el.classList.toggle('marked');
+        });
+        
+    }
+
+    useEffect(() => {
+        getDentalCharts();
+      }, []);
+      
 
     return (
         <>
