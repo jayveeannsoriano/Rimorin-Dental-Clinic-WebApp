ce065aaac3512aafb2bec855637dec340993d036
Dabe Ventigan <2190962@slu.edu.ph>
2022-11-15 00:15:50 +0800

    transaction function, currently working on patient info

--- client/src/pages/patient-records/patient-module/patient-info-edit.jsx
+++ client/src/pages/patient-records/patient-module/patient-info-edit.jsx
@@ -4,6 +4,60 @@ import "react-bootstrap";
 import ProfileWidget from "../../../components/profile-widget";
 
 const PatientInfoEdit = () => {
+  var userInfo = JSON.parse(window.localStorage.getItem('current-session'));
+  const patientIDnumber = userInfo['patientIDnumber'];
+  console.log(patientIDnumber);
+
+  const [firstName, setFirstValue] = useState("");
+  const [lastName, setLastValue] = useState("");
+  const [middleName, setMiddleValue] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [ageValue, setAgeValue] = useState("");
+  const [genderValue, setGenderValue] = useState("");
+  const [professionValue, setProfessionValue] = useState("");
+  const [cellNumber, setCellValue] = useState("");
+  const [tellNumber, setTellNumber] = useState("");
+  const [bloodType, setBloodValue] = useState("");
+  const [houseNum, setHouseValue] = useState("");
+  const [cityValue, setCityValue] = useState("");
+  const [countryValue, setCountryValue] = useState("");
+  const [brgyValue, setBrgyValue] = useState("");
+  const [provinceValue, setProvinceValue] = useState("");
+  const [zipValue, setZipValue] = useState("");
+  const [medValue, setMedValue] = useState("");
+  const [allergiesValue, setAllergiesValue] = useState("");
+  const [condValue, setCondValue] = useState("");
+
+  const updatePatientInfo = () =>{
+    Axios.put("http://localhost:3001/updatePatientInfo",{
+
+     patientIDnumber: patientIDnumber,
+
+     firstName:firstName,
+     lastName:lastName,
+     middleName:middleName,
+     birthDate:birthDate,
+     ageValue:ageValue,
+     genderValue:genderValue,
+     professionValue:professionValue,
+     cellNumber:cellNumber,
+     tellNumber:tellNumber,
+     bloodType:bloodType,
+     houseNum:houseNum,
+     cityValue:cityValue,
+     countryValue:countryValue,
+     brgyValue:brgyValue,
+     provinceValue:provinceValue,
+     zipValue:zipValue,
+     medValue:medValue,
+     allergiesValue: allergiesValue,
+     condValue:condValue
+     });
+    setModalState("modal-2");
+  }
+
+
+
   return (
     <>
       <div class="pagetitle">
@@ -58,7 +112,7 @@ const PatientInfoEdit = () => {
                     First Name
                   </div>
                   <div class="col-lg-auto col-md-auto">
-                    <input name="firstName" type="text" class="form-control" id="firstName" value="Ricci" />
+                    <input name="firstName" type="text" class="form-control" id="firstName" onChange={(e) => setFirstValue(e.target.value)} />
                   </div>
 
                 </div>
@@ -68,28 +122,28 @@ const PatientInfoEdit = () => {
                     Last Name
                   </div>
                   <div class="col-lg-auto col-md-auto">
-                    <input name="lastName" type="text" class="form-control" id="lastName" value="Blynthe" />
+                    <input name="lastName" type="text" class="form-control" id="lastName" onChange={(e) => setLastValue(e.target.value)} />
                   </div>
                 </div>
 
                 <div class="row">
                   <div class="col-lg-auto col-md-auto label">Middle Initial</div>
                   <div class="col-lg-auto col-md-auto">
-                    <input name="middleName" type="text" class="form-control" id="middleName" value="Fuentes" />
+                    <input name="middleName" type="text" class="form-control" id="middleName" onChange={(e) => setMiddleValue(e.target.value)} />
                   </div>
                 </div>
 
                 <div class="row">
                   <div class="col-lg-auto col-md-auto label">Birthdate</div>
                   <div class="col-lg-auto col-md-auto">
-                    <input name="date" type="text" class="form-control" id="Birthday" value="01/01/1998" />
+                    <input name="date" type="text" class="form-control" id="Birthday" onChange={(e) => setBirthDate(e.target.value)} />
                   </div>
                 </div>
 
                 <div class="row">
                   <div class="col-lg-auto col-md-auto label">Age</div>
                   <div class="col-lg-auto col-md-auto">
-                    <input name="age" type="text" class="form-control" id="Birthday" value="24" />
+                    <input name="age" type="text" class="form-control" id="Birthday" onChange={(e) => setAgeValue(e.target.value)} />
                   </div>
                 </div>
 
@@ -97,7 +151,7 @@ const PatientInfoEdit = () => {
                 <div class="row">
                   <div class="col-lg-auto col-md-auto label">Gender</div>
                   <div class="col-lg-auto col-md-auto">
-                    <label>
+                    <label onChange={(e) => setGenderValue(e.target.value)}>
                       <input
                         type="radio"
                         // onChange={handleChange('gender')}
@@ -123,28 +177,28 @@ const PatientInfoEdit = () => {
                 <div class="row">
                   <div class="col-lg-auto col-md-auto label">Profession</div>
                   <div class="col-lg-auto col-md-auto">
-                  <input name="profession" type="text" class="form-control" id="Profession" value="Student" />
+                  <input name="profession" type="text" class="form-control" id="Profession" onChange={(e) => setProfessionValue(e.target.value)} />
                   </div>
                 </div>
 
                 <div class="row">
                   <div class="col-lg-auto col-md-auto label">Cell #</div>
                   <div class="col-lg-auto col-md-auto">
-                  <input name="cellNumber" type="tel" class="form-control" id="cellNumber" value="Student" />
+                  <input name="cellNumber" type="tel" class="form-control" id="cellNumber" onChange={(e) => setCellValue(e.target.value)} />
                   </div>
                 </div>
 
                 <div class="row">
                   <div class="col-lg-auto col-md-auto label">Tel #</div>
                   <div class="col-lg-auto col-md-auto">
-                    <input name="telNumber" type="tel" class="form-control" id="telNumber" value="N/A" />
+                    <input name="telNumber" type="tel" class="form-control" id="telNumber" onChange={(e) => setTellNumber(e.target.value)} />
                   </div>
                 </div>
 
                 <div class="row">
                   <div class="col-lg-auto col-md-auto label">Blood Type</div>
                   <div class="col-lg-auto col-md-auto">
-                    <input type="bloodType" type="text" class="form-control" id="bloodType" value="A/B"/>
+                    <input type="bloodType" class="form-control" id="bloodType" onChange={(e) => setBloodValue(e.target.value)}/>
                   </div>
                 </div>
               </div>
@@ -159,7 +213,7 @@ const PatientInfoEdit = () => {
                   House No. & Street Name
                 </div>
                 <div class="col-lg-auto col-md-auto">
-                  <input type="address" type="text" class="form-control" id="Address" value="#10 Cirineo Subdivision"/>
+                  <input type="address-house" class="form-control" id="Address" onChange={(e) => setHouseValue(e.target.value)}/>
                 </div>
               </div>
 
@@ -168,14 +222,14 @@ const PatientInfoEdit = () => {
                   Municipality/City
                 </div>
                 <div class="col-lg-auto col-md-auto">
-                  <input type="address" type="text" class="form-control" id="Address" value="Dagupan"/>
+                  <input type="address-city" class="form-control" id="Address" onChange={(e) => setCityValue(e.target.value)}/>
                 </div>
               </div>
 
               <div class="row">
                 <div class="col-lg-auto col-md-auto label">Country</div>
                 <div class="col-lg-auto col-md-auto">
-                  <input type="address" type="text" class="form-control" id="Address" value="Philippines"/>
+                  <input type="address" class="form-control" id="Address" onChange={(e) => setCountryValue(e.target.value)}/>
                 </div>
               </div>
 
@@ -183,20 +237,20 @@ const PatientInfoEdit = () => {
               <div class="row">
                 <div class="col-lg-auto col-md-auto label">District/Barangay</div>
                 <div class="col-lg-auto col-md-auto">
-                  <input type="address" type="text" class="form-control" id="Address" value="Tapuac"/>
+                  <input type="address" class="form-control" id="Address" onChange={(e) => setBrgyValue(e.target.value)}/>
                 </div>
               </div>
 
               <div class="row">
                 <div class="col-lg-auto col-md-auto label">Province</div>
                 <div class="col-lg-auto col-md-auto">
-                  <input type="address" type="text" class="form-control" id="Address" value="Pangasinan"/>
+                  <input type="address" class="form-control" id="Address" onChange={(e) => setProvinceValue(e.target.value)}/>
                 </div>
               </div>
               <div class="row">
                 <div class="col-lg-auto col-md-auto label">ZIP Code</div>
                 <div class="col-lg-auto col-md-auto">
-                  <input type="address" type="text" class="form-control" id="Address" value="2400"/>
+                  <input type="address" class="form-control" id="Address" onChange={(e) => setZipValue(e.target.value)}/>
                 </div>
               </div>
 
@@ -210,7 +264,7 @@ const PatientInfoEdit = () => {
                 </div>
 
                 <div class="col-lg-3">
-                  <input type="medications" type="text" class="form-control" id="Medications" value="N/A"/>
+                  <input type="medications" class="form-control" id="Medications" onChange={(e) => setMedValue(e.target.value)}/>
                 </div>
               </div>
 
@@ -220,7 +274,7 @@ const PatientInfoEdit = () => {
                   Ibuprofen
                 </div> */}
                 <div class="col-lg-3">
-                  <input type="allergies" type="text" class="form-control" id="Allergies" value="Ibuprofen"/>
+                  <input type="allergies" class="form-control" id="Allergies" onChange={(e) => setAllergiesValue(e.target.value)}/>
                 </div>
               </div>
 
@@ -230,12 +284,17 @@ const PatientInfoEdit = () => {
                   Asthma
                 </div> */}
                 <div class="col-lg-3">
-                  <input type="conditions" type="text" class="form-control" id="Conditions" value="Asthma"/>
+                  <input type="conditions" class="form-control" id="Conditions" onChange={(e) => setCondValue(e.target.value)}/>
                 </div>
               </div>
 
             </div>
             {/* end of card body */}
+            <button className="btn btn-primary" type="submit" onClick={updatePatientInfo()}>
+                  <i class="bi bi-save"></i>
+                  Save
+                </button>
+                <div className="divider"></div>
           </div>
         </div>
 
