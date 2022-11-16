6f11162e3b36232b40b16993ebaf2bc0e962ec28
John Lloyd Balcita <2195929@slu.edu.ph>
2022-11-17 01:46:11 +0800

    added get data and display in view dental record

--- client/src/pages/patient-records/view-dental-record.jsx
+++ client/src/pages/patient-records/view-dental-record.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/dental-record.css"
 import PatientProfileWidget from "../../components/profile-widget";
 import DentalRecordDataTable from "../../components/patient-dataTables/dentalrecord-datatable";
+import DentalChart from "../../components/dental-teeth-chart";
+import axios from "axios";
+
 
 const ViewDentalRecord = () => {
+    // const getPatientDetails = async() => {
+    //     try{
+    //         const response = await Axios.get('http://localhost:3001/getUserDetails');
+    //         console.log(response, "Responses");
+    //         setPatientList(response.data);
+    //     }catch (error){
+    //         console.log(error)
+    //     }
+    // }
+
+    const [recordData,setRecordData] = useState([]);
+    const [recordProcedures,setProcedures] = useState([]);
+    var url = require('url');
+    var url_parts = url.parse(window.location.href, true);
+    var query = url_parts.query;
+
+    const getDentalCharts = async() => {
+        // var url = require('url');
+        // var url_parts = url.parse(window.location.href, true);
+        // var query = url_parts.query;
+        const response = await axios.get('http://localhost:3001/getSpecificDentalRecord', { params: { patientIDNum: query.patientIDNum,date:query.date} });
+        setRecordData(response.data);
+        const chartedTeeth = response.data.chartedTeeth; 
+        const procedures = response.data.procedures; 
+        chartedTeeth.forEach(teeth => {
+            var el = document.getElementById(teeth);
+            el.classList.toggle('marked');
+        });
+        
+        procedures.forEach(procedure => {
+            if(procedure.chosen!=null){
+                recordProcedures.push(procedure.chosen);
+            }
+        });
+    
+    }
+
+    useEffect(() => {
+        // const email =  document.getElementById('emailaddress').textContent;
+
+        getDentalCharts();
+    }, []);
+
+
+    
     return(
         <>
                 <div class="pagetitle">
@@ -38,7 +86,7 @@ const ViewDentalRecord = () => {
 
                                     {/* Dental Teeth Chart */}
                                     <h5 className="card-title">Dental Chart</h5>
-
+                                    <DentalChart/>
                                     <div class="row">
                                         {/* Insert Dental Teeth Chart for selected patient*/}
                                     </div>
@@ -48,28 +96,29 @@ const ViewDentalRecord = () => {
                                     <div class="row">
                                         <div className="col-3">
                                             <h6>Selected Tooth/Teeth</h6>
-                                            <p>16</p>
+                                            <p>{recordData.chartedTeeth}</p>
                                         </div>
                                         <div className="col-3">
                                             <h6>Date of Treatment</h6>
-                                            <p>November 6, 2022</p>
+                                            <p>{recordData.dentalDate}</p>
                                         </div>
                                         <div className="col-3">
                                             <h6>Treatment Description</h6>
-                                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                                            <p>{recordData.dentalDesc}</p>
                                         </div>
                                         <div className="col-3">
                                             <h6>Procedure/s</h6>
-                                            <p>Root Canal Therapy</p>
+                                            <p>{recordProcedures.toString()}</p>
                                         </div>
                                     </div>
 
                                     {/* Treatment Attatchments */}
-                                    <h5 className="card-title">Treatment Attatchments</h5>
+                                    <h5 className="card-title">Treatment Attachments</h5>
                                     <div className="row attatchment-container" 
                                     style={{border: "2px solid #885df1", borderRadius: "15px", width: "784px", height: "289px"}}>
                                         yan lang yung progress niyo?
                                         {/* Insert uploaded image/s for that specific record*/}
+                                        <img src={"../../../../../uploads/dental-record-images/" + query.patientIDNum + "_" + query.date + ".jpg"} alt="Treatment attatchment"/>
                                     </div>
 
                                 </div>
