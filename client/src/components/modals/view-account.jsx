a2b53dd63e84002ae227331acfda8ba6ac6cc1b2
Quina Mae <2190678@slu.edu.ph>
2022-11-19 02:37:37 +0800

    admin datatable, view and archive modals

---
+++ client/src/components/modals/view-account.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import '../../styles/modals.css';
+import Form from 'react-bootstrap/Form';
+
+function ViewAccount (pName, consultation, appNum, date, time, appStats) {
+    const [modalState, setModalState] = useState('close');
+    const handleClose = () => setModalState(false);
+    const handleModal1 = () => {
+        setModalState("modal-1")
+    }
+    //Get values 
+    
+    return (
+        <>
+            <Button className="view-button" variant="primary" onClick={handleModal1}>
+                <i class="bi bi-eye-fill"></i> View
+            </Button>
+
+            <Modal
+                show={modalState == 'modal-1'}
+                size="lg"
+                // show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Account Details</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body className="show-grid">
+                    <div className="appointment-details-modal">
+                        <div class="row">
+                            <div class="col modal-label">Lorem</div>
+                            <div class="col modal-values">Text</div>
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default ViewAccount;
