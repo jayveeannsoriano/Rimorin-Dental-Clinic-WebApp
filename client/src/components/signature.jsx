4cc2958ca1171a56303cbd5c3a26b06936a9f5fe
Mr-Benzedrine <2194280@slu.edu.ph>
2022-11-21 18:11:42 +0800

    updated e-prescription signature validation

--- client/src/components/signature.jsx
+++ client/src/components/signature.jsx
@@ -48,7 +48,7 @@ const Signature = props => {
                     <p><span>Upload a file </span>or drag & signature</p>
                     <p>PNG, JPG, JPEG up to 5MB</p>
                 </div>
-                <input type="file" name="imageFile" multiple onChange={onFileDrop}/>
+                <input type="file" name="imageFile" multiple onChange={onFileDrop} required/>
             </div>
             {
                 fileList.length > 0 ? (
