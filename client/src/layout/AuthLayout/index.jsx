ef7027c0e05443f16257815cb38fb6c6db7d02c4
Jayvee Ann Soriano <111727585+jayveeannsoriano@users.noreply.github.com>
2022-11-23 19:11:55 +0800

    minor fixes in login/signup

--- client/src/layout/AuthLayout/index.jsx
+++ client/src/layout/AuthLayout/index.jsx
@@ -6,21 +6,14 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <>
-      <main>
-        <header id="headerlogin" className="fixed-top">
-          <div className="container d-flex align-items-center">
-            <h1 className="logo me-auto">
-              <a href="/">Rimorin Dental Clinic</a>
-            </h1>
-          </div>
-        </header>
+      <main className="landing-main">
         <section id="auth-section" className="auth-section">
           <div className="container auth-container">
             <div className="row">
-              <div className="col-lg-6 col-xl-6">
+              <div className="col-lg-6 col-xl-6 col-md-6">
                 <img src={"../img/hero-img.png"} className="img-fluid" />
               </div>
-              <div className="col-lg-6 col-xl-6 p-3 justify-content-center">
+              <div className="col-lg-6 col-xl-6 col-md-6 p-3 justify-content-center">
                 <Outlet />
               </div>
             </div>
