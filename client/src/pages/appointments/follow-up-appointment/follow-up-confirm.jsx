46d8d58cc59bac095b6941e9ac07df84fee65d17
John Lloyd Balcita <2195929@slu.edu.ph>
2022-11-28 00:56:07 +0800

    added 63 format to movider texting

--- client/src/pages/appointments/follow-up-appointment/follow-up-confirm.jsx
+++ client/src/pages/appointments/follow-up-appointment/follow-up-confirm.jsx
@@ -35,7 +35,7 @@ const BookingConfirm = ({nextStep, prevStep, values}) => {
     Axios.post("https://rimorin-dental-clinic.herokuapp.com/insertFollowUpAppointment", {patientIDnumber: patientIDNum ,docID:docID,docName:docName, prevAppNum:prevAppNum, userNameApp: getUserName, startDate: date,formattedDate:formattedDate, consulInput: values.consultation, getTime:time, recep:getUserEmail})
 
     fetch("https://cors-anywhere.herokuapp.com/https://api.movider.co/v1/sms", {
-        body: "api_key=9rcBz4qgXLHOeilJ7OQwGFvlW8H3-X&api_secret=9bW6Qe6tNi4jyJ0a5RfzuqYS_oZqIA&to="+getUserPhone+ "&text= Hi "+getUserName+"! This is from Rimorin Dental Clinic notifying you of your requested Appointment at "+date+" "+time+" due to '" + values.consultation + "'. See you there!",
+        body: "api_key=9rcBz4qgXLHOeilJ7OQwGFvlW8H3-X&api_secret=9bW6Qe6tNi4jyJ0a5RfzuqYS_oZqIA&to=63"+getUserPhone+ "&text= Hi "+getUserName+"! This is from Rimorin Dental Clinic notifying you of your requested Appointment at "+date+" "+time+" due to '" + values.consultation + "'. See you there!",
         headers: {
             Accept: "application/json",
             "Content-Type": "application/x-www-form-urlencoded",
