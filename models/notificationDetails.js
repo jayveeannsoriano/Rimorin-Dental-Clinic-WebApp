d9dbbeedc4e6f9e8a5bc2d1bfc5065f3cdb6017f
Dabe Ventigan <2190962@slu.edu.ph>
2023-02-08 17:42:28 +0800

    new file:   models/notificationDetails.js

---
+++ models/notificationDetails.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const NotificationDetails = new mongoose.Schema(
+  {
+    pName: String,
+    dName: String,
+    appNum: String,
+    date: String,
+    time: String,
+    currentDate: String,
+    prescriptionNum: String,
+    sName: String,
+    patientIDnumber: String,
+  },
+  {
+    collection: "NotificationDetails",
+  },
+);
+
+const NotifDetailsDB = mongoose.model("NotificationDetails", NotificationDetails);
+module.exports = NotifDetailsDB;
+	
